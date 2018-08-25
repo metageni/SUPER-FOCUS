1dbd838d33f528e50676a8940c13fa3556250956
Geni Silva <geni.silva@helix.com>
2018-08-25 11:11:02 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ setup_requirements = [
 
 setup(
     name = 'superfocus',
-    version = 0.31,
+    version = 0.32,
     description = 'SUPER-FOCUS: A tool for agile functional analysis of shotgun metagenomic data',
     author = 'Genivaldo G.Z. Silva',
     author_email = 'genivaldo.gueiros@gmail.com',
